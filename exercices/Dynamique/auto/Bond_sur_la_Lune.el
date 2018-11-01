cb097be0000d6f142abc488604b4f4b96607818e
Pierre-antoine Comby <comby@crans.org>
2018-11-01 16:37:20 +0100

    update exercises

--- exercices/Dynamique/auto/Bond_sur_la_Lune.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "Bond_sur_la_Lune"
- (lambda ()
-   (LaTeX-add-environments
-    '("cases" LaTeX-env-args ["argument"] 0)))
- :latex)
-
--- exercices/Forces_centrales/auto/Limite_de_Roche.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "Limite_de_Roche"
- (lambda ()
-   (LaTeX-add-environments
-    '("cases" LaTeX-env-args ["argument"] 0)))
- :latex)
-
